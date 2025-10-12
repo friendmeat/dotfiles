32335ed5d60f169939045d77b6b66baed7b61e36
friendmeat <friendmeat@proton.me>
2025-10-12 11:42:21 -0400

    link.sh and README tweak

--- README.md
+++ README.md
@@ -11,14 +11,9 @@ cd ~/dotfiles
 
 2. Link dotfiles
 ```sh
-chmod +x link.sh
 ./link.sh
 ```
 
 # Development
 
-1. Add a new line to `link.sh` for every dotfile. For example:
-
-```sh
-ln -s "${SCRIPTPATH}/.my_dotfile" ~/.my_dotfile
-```
+Add a new file to the `CONFIGS` array in `link.sh` for every dotfile.
--- link.sh
+++ link.sh
@@ -2,7 +2,14 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
-CONFIGS=('.profile' '.bashrc' '.gitconfig' '.zshrc' '.bash_aliases')
+CONFIGS=(
+    '.profile' 
+    '.bashrc' 
+    '.gitconfig' 
+    '.zshrc' 
+    '.bash_aliases'
+)
+
 for config in ${CONFIGS[@]}; do
-		ln -s "${SCRIPTPATH}/${config}" ~/"${profile}"
+		ln -s "${SCRIPTPATH}/${config}" "${HOME}/${profile}"
 done
