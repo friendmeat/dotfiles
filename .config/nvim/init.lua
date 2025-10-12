20835f7d32ca7b9c23690c834e536fd3df640df6
friendmeat <friendmeat@proton.me>
2025-10-12 11:41:14 -0400

    re-add telescope keymap and format-on-save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,87 @@
 -- PLUGINS --
 require("config.lazy")
 require("nvim-tree").setup()
-require("toggleterm").setup {
+require("toggleterm").setup({
 	open_mapping = [[<c-\>]],
-	direction = 'horizontal'
-}
+	direction = "horizontal",
+})
 
 -- LSPs --
--- local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocl.make_client_capabilities())
 
-vim.lsp.config("markdown_oxide", {
-		-- capabilities = vim.tbl_deep_extend('force', capabilities, { workspace = { didChangeWatchedFiles = { dynamicRegistration = true, }, }, }),
-		-- on_attach = on_attach
+vim.lsp.config("markdown_oxide", {})
+
+-- Bind keys when lsp attached to buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function()
+		local bufmap = function(mode, lhs, rhs)
+			local opts = { buffer = true }
+			vim.keymap.set(mode, lhs, rhs, opts)
+		end
+		-- Format document
+		-- bufmap("n", "<space>f", "<cmd>lua vim.lsp.buf.format()<cr>")
+		-- Jump to the definition
+		bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+		-- Jump to declaration
+		bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+		-- Lists all the implementations for the symbol under the cursor
+		bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
+		-- Jumps to the definition of the type symbol
+		bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+		-- Lists all the references
+		bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
+		-- Displays a function's signature information
+		bufmap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
+		-- Renames all references to the symbol under the cursor
+		bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
+		-- Selects a code action available at the current cursor position
+		bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+		-- Show diagnostics in a floating window
+		bufmap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+		-- Move to the previous diagnostic
+		bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+		-- Move to the next diagnostic
+		bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+	end,
+})
+
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		vim.lsp.buf.format({ async = true })
+	end,
 })
 
 -- KEY BINDINGS --
 -- Rebind window navigation commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Telescope
+local telescope = require("telescope")
+telescope.setup({
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 -- OPTIONS --
 vim.o.number = true
 vim.o.relativenumber = true
+
+-- Tabs
+vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 
 -- disable netrw in favor of nvim-tree
@@ -32,4 +89,4 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- COLOR SCHEME --
-vim.cmd.colorscheme('everforest')
+vim.cmd.colorscheme("everforest")
